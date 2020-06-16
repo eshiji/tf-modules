beb44cf38a0dfc8ad64884c73821130e8227ade1
eshijii <eshijii@gmail.com>
2020-06-16 19:04:18 -0300

    Initial commit for tests purposes

---
+++ aws/teste/variables.tf
@@ -0,0 +1,8 @@
+variable "aws_local_profile" {}
+
+variable "aws_region" {}
+
+variable "ami_id" {}
+
+variable "instance_type" {}
+
---
+++ main.tf
@@ -0,0 +1,9 @@
+provider "aws" {
+  profile = var.local_profile
+  region  = var.aws_region
+}
+
+resource "aws_instance" "example" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+}
