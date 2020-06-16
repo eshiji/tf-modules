ca7adcd5c1e911def1af694ce508f1747d4ee625
eshijii <eshijii@gmail.com>
2020-06-16 19:44:42 -0300

    adding terraform cloud as remote backend

--- aws/teste/main.tf
+++ aws/teste/main.tf
@@ -3,6 +3,17 @@ provider "aws" {
   region  = var.aws_region
 }
 
+terraform {
+  backend "remote" {
+    organization = "eshiji"
+    hostname = "app.terraform.io"
+
+    workspaces {
+      name = "tf-modules"
+    }
+  }
+}
+
 resource "aws_instance" "example" {
   ami           = var.ami_id
   instance_type = var.instance_type
