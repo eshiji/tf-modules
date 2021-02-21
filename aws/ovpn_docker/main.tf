48bcad92d6306edf0c320a3c2a1ef27d24a22714
Rafael Miazaki <eshijii@gmail.com>
2021-02-21 20:24:48 -0300

    changing udp to tcp

--- aws/ovpn_docker/main.tf
+++ aws/ovpn_docker/main.tf
@@ -100,7 +100,7 @@ resource "aws_security_group" "ec2_sg" {
     description = "SSH"
     from_port   = 22
     to_port     = 22
-    protocol    = "udp"
+    protocol    = "tcp"
     cidr_blocks = var.cidr_block_whitelist
   }
 
